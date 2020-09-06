4b9d118142e51e3eaa16efda5a6c21164f446675
Sowndarya2295 <69381952+Sowndarya2295@users.noreply.github.com>
2020-09-06 20:05:40 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,29 @@
+class Paper {
+    constructor(x,y,r) {
+      var options = {
+          'isStatic':false,
+          'restitution':0.3,
+          'friction':0.5,
+          'density':1.2
+      }
+      this.y = y;
+      this.x = x;
+      this.r = r;
+
+      this.body = Bodies.circle(this.x,this.y,this.r/2, options);
+      this.image = loadImage("images/paperProject.png");
+      
+      World.add(world, this.body);
+    }
+    display(){
+      
+      push();
+      translate(this.body.position.x,this.body.position.y);
+      strokeWeight(4);
+      stroke("pink");
+      fill("white");
+      image(this.image,0,0,this.r,this.r);
+      pop();
+    }
+  }
+  
